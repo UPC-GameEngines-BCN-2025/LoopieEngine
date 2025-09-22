be3e36c0ce72e39e512ecc70680e44f8ff1fbd89
XeivUPC <145395703+XeivUPC@users.noreply.github.com>
2025-09-22 14:45:23 +0200

    imgui integration

--- Loopie/src/Loopie/Core/Assert.h
+++ Loopie/src/Loopie/Core/Assert.h
@@ -5,12 +5,7 @@
 #include <string>
 #include <filesystem>
 
-
 namespace Loopie {
+#define ASSERT(check, msg, ...) if (check){ Log::Critical((std::string("(") + std::filesystem::path(__FILE__).filename().string() + ":" + std::to_string(__LINE__) + ") -> " + msg).c_str(), ##__VA_ARGS__); __debugbreak(); }
 
-#define ASSERT(check, msg, ...) if (check){ Log::Critical((std::string("(") + std::filesystem::path(__FILE__).filename().string() + ":" + STRINGIFY(__LINE__) + ") -> " + msg).c_str(), ##__VA_ARGS__); __debugbreak(); }
-
-
-#define STRINGIFY_DETAIL(x) #x
-#define STRINGIFY(x) STRINGIFY_DETAIL(x)
 }
